8434ed53553a78c84f9df6e8a1ef74fc46bbe9eb
dholub <holubdenis2010@gmail.com>
2025-09-25 20:05:12 +0300

    Знов я помилився з debug.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
+DEBUG = DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
